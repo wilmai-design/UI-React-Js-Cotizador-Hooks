5883d22d269a3da51199347eb8d04a730e8ce039
Luis Perez Mosteiro <lperez@ntb-158.d01.keplertech.com>
2020-01-21 16:15:59 -0300

    Construyendo Hooks para el formulario y los select

---
+++ src/componentes/Formulario.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda'
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #fff;
+    transition: .5s ease;
+
+    &:hover {
+        background-color: #326ac0;
+        cursor: pointer;
+    }
+`;
+
+const Fomulario = () => {
+
+    // Utilizar useMoneda
+    const [ moneda, SelectMonedas, actualizarState ] = useMoneda();
+
+    return ( 
+        <form>
+            <SelectMonedas />
+
+            <Boton
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+    );
+}
+
+export default Fomulario;
