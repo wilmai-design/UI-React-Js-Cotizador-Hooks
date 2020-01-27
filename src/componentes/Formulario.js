a833ce715a68dce47cce90f16afd67ca4db6a4a9
Luis Perez Mosteiro <lperez@ntb-158.d01.keplertech.com>
2020-01-27 17:33:28 -0300

    Validando los datos del fomulario despues de apretar submit

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+
+import Error from './Error';
 import useMoneda from '../hooks/useMoneda'
 import useCriptomoneda from '../hooks/useCriptomonedas';
-// import { useEffect } from 'react';
 import axios from 'axios';
 
 const Boton = styled.input`
@@ -27,6 +28,7 @@ const Fomulario = () => {
 
     // state del listado de criptomoneda
     const [ listacripto, guardarCriptomonedas ] = useState([]);
+    const [ error, guardarError ] = useState(false);
 
     const MONEDAS = [
         { codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
@@ -54,10 +56,28 @@ const Fomulario = () => {
         consultartAPI();
     }, []);
 
+    // Cuando el susuario haga submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        // validar si ambos campos estan llenos
+        if(moneda === '' || criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        // pasar los datos al componente principal
+        guardarError(false);
+    }
 
 
     return ( 
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+
+            {error ? <Error mensaje="Todos los campos son obligatorios" />: null }
+
             <SelectMonedas />
 
             <SelectCriptomoneda />
